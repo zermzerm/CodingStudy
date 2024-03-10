8e3499d2bd5a055d3f04c178be1c4b20964a8a6c
oauch <lgm9515@naver.com>
2024-03-10 20:49:00 +0900

    PGS: 수열과 구간 쿼리 4

---
+++ oauch/20240311/수열과 구간 쿼리 4.js
@@ -0,0 +1,10 @@
+/**
+ * URL https://school.programmers.co.kr/learn/courses/30/lessons/181922
+ * 문제 이름 : 수열과 구간 쿼리 4
+ * 해결 실패 (예전부터 못 풀었던 문제입니다 🥺)
+ */
+
+function solution(arr, queries) {
+  var answer = [];
+  return answer;
+}
