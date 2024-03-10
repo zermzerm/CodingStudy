ec8738b9a68e74c2765a3a0ac5f1b66e9cd9237b
Yeon Kim <46954114+arky02@users.noreply.github.com>
2024-03-11 03:30:37 +0900

    PGS: 리스트 자르기.js

--- arky02/20240311/리스트 자르기.js
+++ arky02/20240311/리스트 자르기.js
@@ -8,3 +8,25 @@ function solution(n, slicer, num_list) {
         
     }
 }
+
+
+/*
+이건 왜 않됨?;;; 테스트 19만 틀렸다고 나옴;;ㅠ.ㅠ 물론 코드는 구리지만 ㅎㅎ
+function solution(n, slicer, num_list) {
+    var answer = [];
+    const [a,b,c] = [slicer[0],slicer[1],slicer[2]];
+    if(n === 4){
+        const ans = num_list.filter((num, index)=> 
+            index>=a && index<=b && (num-num_list[a])%c === 0
+        )
+        answer = ans;
+    }
+    switch(n){
+        case 1: return num_list.slice(0,b+1);
+        case 2: return num_list.slice(a);
+        case 3: return num_list.slice(a, b+1);
+        
+    }
+    return answer;
+}
+*/
