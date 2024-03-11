2dc34d25b6c25e745202245ad0a2de31bbafb6c3
박경서 <zerm0907@gmail.com>
2024-03-11 17:36:06 +0900

    PGS : 없는 숫자 더하기

---
+++ zermzerm/20240313/없는 숫자 더하기.js
@@ -0,0 +1,13 @@
+/**
+ * URL: https://school.programmers.co.kr/learn/courses/30/lessons/86051
+ * 문제 풀이 포인트: for 만세, 1부터 9까지의 합
+ * 나의 실수: X
+ */
+
+function solution(numbers) {
+  var sum = 0;
+  for (i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return 45 - sum;
+}
