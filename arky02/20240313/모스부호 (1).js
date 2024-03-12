c936e350fedbee67c6df8f2278ae5777407e2512
Yeon Kim <46954114+arky02@users.noreply.github.com>
2024-03-13 02:14:43 +0900

    PGS: 모스부호 (1)

---
+++ arky02/20240313/모스부호 (1).js
@@ -0,0 +1,15 @@
+function solution(letter) {
+    var answer = '';
+    const morse = { 
+        '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+        '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+        '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+        '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+        '-.--':'y','--..':'z'
+    }
+    const letterArr = letter.split(" ");
+    for(let singleMorse of letterArr){
+        answer += morse[singleMorse]
+    }
+    return answer;
+}
