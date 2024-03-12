cd8e93b6cbb40e309cf9e6709f7a8afc5bac633e
Yeon Kim <46954114+arky02@users.noreply.github.com>
2024-03-13 02:22:02 +0900

    PGS: 주석 추가

--- arky02/20240313/핸드폰 번호 가리기.js
+++ arky02/20240313/핸드폰 번호 가리기.js
@@ -1,3 +1,9 @@
+/**
+ * URL https://school.programmers.co.kr/learn/courses/30/lessons/12948
+ * 문제 이름 : 핸드폰 번호 가리기
+ * slice 사용에 익숙치 않다.
+ */
+
 function solution(phone_number) {
     let answer = '';
     for(let i = 0; i< phone_number.length-4; i++){
