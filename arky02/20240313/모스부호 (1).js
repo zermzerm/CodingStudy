fb94eb2695a59102c0a76ed0840934ab9af5480c
Yeon Kim <46954114+arky02@users.noreply.github.com>
2024-03-13 02:18:18 +0900

    PGS: 모스부호 주석 추가

--- arky02/20240313/모스부호 (1).js
+++ arky02/20240313/모스부호 (1).js
@@ -1,3 +1,9 @@
+/**
+ * URL https://school.programmers.co.kr/learn/courses/30/lessons/120838
+ * 문제 이름 : 모스부호 (1)
+ * 음 쉬웠다
+ */
+
 function solution(letter) {
     var answer = '';
     const morse = { 
