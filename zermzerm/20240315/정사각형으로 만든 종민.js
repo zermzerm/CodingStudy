135c9fd3a1fafde27a7b953ee18805f693148767
박경서 <zerm0907@gmail.com>
2024-03-13 18:07:01 +0900

    PGS : 정사각형으로 만들기

---
+++ zermzerm/20240315/정사각형으로 만든 종민.js
@@ -0,0 +1,21 @@
+/**
+ * URL: https://school.programmers.co.kr/learn/courses/30/lessons/12906
+ * 문제 풀이 포인트: if?
+ * 나의 실수: 고차원 배열의 깊은 복사
+ */
+
+function solution(arr) {
+  let answer = arr.map((v) => [...v]);
+  if (arr.length === arr[0].length) return answer;
+  else if (arr.length > arr[0].length) {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length - arr[0].length; j++) {
+        answer[i].push(0);
+      }
+    }
+  } else if (arr.length < arr[0].length) {
+    for (let i = 0; i < arr[0].length - arr.length; i++)
+      answer.push(new Array(arr[0].length).fill(0));
+  }
+  return answer;
+}
