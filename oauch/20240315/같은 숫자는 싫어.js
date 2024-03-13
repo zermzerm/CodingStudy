54e4094a8b1bb41a914167c859c8d7ab33964534
oauch <lgm9515@naver.com>
2024-03-13 13:39:02 +0900

    PGS: 같은 숫자는 싫어

--- oauch/20240315/같은 숫자는 싫어.js
+++ oauch/20240315/같은 숫자는 싫어.js
@@ -1,6 +1,6 @@
 /**
- * URL https://school.programmers.co.kr/learn/courses/30/lessons/12950
- * 문제 이름 : 행렬의 덧셈
+ * URL https://school.programmers.co.kr/learn/courses/30/lessons/12906
+ * 문제 이름 : 같은 숫자는 싫어
  * 접근 방법 : 배열 arr를 filter 함수를 사용해서 [index에 해당하는 값]과 [index + 1의 값]이 다른거만 출력되도록 했다.
  */
 
