7eb5f7f23b24668ad19f3a72f8e1698229cfbfde
Yeon Kim <46954114+arky02@users.noreply.github.com>
2024-03-15 01:15:58 +0900

    PGS: 주석추가

--- arky02/20240315/JadenCase 문자열 만들기.js
+++ arky02/20240315/JadenCase 문자열 만들기.js
@@ -1,3 +1,9 @@
+/**
+ * URL: https://school.programmers.co.kr/learn/courses/30/lessons/12951#
+ * 문제 풀이 포인트: 알파벳 여부 구별, 공백 문자 연속일 경우 구별, 대소문자 치환 방법
+ * 나의 실수: 솔직히 알파벳 어떻게 구별하는지, 대소문자 치환 어케하는지 찾아봤다. 그리고 공백 숫자 연속일 경우를 생각을 못해서 런타임 에러가 났었다.
+ */
+
 function solution(s) {
     let answer = '';
     const strArr = s.split(" ");
