afb7ce877c34a36ecfbe2bfb0ff3cbd5e4c33836
Soeun Kim <summerkim0825@gmail.com>
2024-03-14 16:37:02 +0900

    PGS: 최댓값과 최솟값 수정

--- summerkimm/20240315/최댓값과 최솟값.js
+++ summerkimm/20240315/최댓값과 최솟값.js
@@ -1,15 +1,15 @@
 /**
  * URL: https://school.programmers.co.kr/learn/courses/30/lessons/12939
- * 풀이 방법: 아래와 같이 풀긴 했으나 비효율적인 느낌.. 
+ * 풀이 포인트 : 아래와 같이 풀긴 했으나 너무 비효율적인 것 같다.. 
  */
 
 function solution(s) {
-  let answer = [];
+  let result = [];
   let array = s.split(" ");
   let min_number = Math.min(...array);
-  answer.push(min_number);
+  result.push(min_number);
   let max_number = Math.max(...array);
-  answer.push(max_number);
-  const result = answer.join(" ");
-  return result;
+  result.push(max_number);
+  const answer = result.join(" ");
+  return answer;
 }
