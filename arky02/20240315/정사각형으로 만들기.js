8a627eca354f6e97d600937290e94cac27de7906
Yeon Kim <46954114+arky02@users.noreply.github.com>
2024-03-15 01:18:56 +0900

    PGS: 주석 추가

--- arky02/20240315/정사각형으로 만들기.js
+++ arky02/20240315/정사각형으로 만들기.js
@@ -1,3 +1,9 @@
+/**
+ * URL https://school.programmers.co.kr/learn/courses/30/lessons/12906
+ * 문제 이름 : 정사각형으로 만들기
+ * 문제점 : 처음에 이중 for문으로 일단 전체 배열 값 다 돌면서, if로 조건 비교하면서 요소 추가하려했다가 빠른 손절치고 먼저 행과 열의 값 중에 어떤게 큰지 찾고 이걸로 분기를 나눈게 다행인 것 같다.
+ */
+
 function solution(arr) {
     let answer = arr;
     
