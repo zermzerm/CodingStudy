747661a85d647ad3f8eb72b026dd94b2f18b5c66
Yeon Kim <46954114+arky02@users.noreply.github.com>
2024-03-15 01:17:18 +0900

    PGS: 주석 추가

--- arky02/20240315/같은 숫자는 싫어.js
+++ arky02/20240315/같은 숫자는 싫어.js
@@ -1,3 +1,9 @@
+/**
+ * URL https://school.programmers.co.kr/learn/courses/30/lessons/12906
+ * 문제 이름 : 같은 숫자는 싫어
+ * 문제점 : 솔직히 쉬웠다. 구글링 안하고 풀음
+ */
+
 function solution(arr)
 {
     let answer = [];
