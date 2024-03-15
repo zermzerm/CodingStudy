d25eb71206e7f27418d7f2f73eb4dc2833606892
Soeun Kim <summerkim0825@gmail.com>
2024-03-15 18:01:12 +0900

    PGS: 정사각형으로 만들기 수정

--- summerkimm/20240315/정사각형으로 만들기.js
+++ summerkimm/20240315/정사각형으로 만들기.js
@@ -2,7 +2,24 @@
  * URL: https://school.programmers.co.kr/learn/courses/30/lessons/181830
  */
 
+function solution(arr) {
+  let row = arr.length;
+  let col = arr[0].length;
 
+  if (row > col) {
+    for (let i = 0; i < row; i++) {
+      for (let j = 0; j < row - col; j++) {
+        arr[i].push(0);
+      }
+    }
+  } else if (row < col) {
+    let zeroArr = new Array(arr[0].length).fill(0);
+    for (let i = 0; i < col - row; i++) {
+      arr.push(zeroArr);
+    }
+  }
+  return arr;
+}
 
 // 처음 시도 -> 정확성 20 -> 이거를 살릴 수 없을까..?
 function solution(arr) {
