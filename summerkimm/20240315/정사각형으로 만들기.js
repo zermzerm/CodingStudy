08323cff0d4cf41dcbc068d6560ae6eff1b74db7
Soeun Kim <summerkim0825@gmail.com>
2024-03-17 20:58:03 +0900

    PGS: 정사각형으로 만들기 수정

--- summerkimm/20240315/정사각형으로 만들기.js
+++ summerkimm/20240315/정사각형으로 만들기.js
@@ -22,17 +22,17 @@ function solution(arr) {
 }
 
 // 처음 시도 -> 정확성 20 -> 이거를 살릴 수 없을까..?
-function solution(arr) {
-  if (arr.length === arr[0].length) return arr;
-  if (arr.length > arr[0].length) {
-    for (let i = 0; i < arr.length; i++) {
-      arr[i].push(0);
-    }
-  } else {
-    let zeroArr = new Array(arr[0].length).fill(0);
-    for (let i = 0; i <= arr[0].length - arr.length; i++) {
-      arr.push(zeroArr);
-    }
-  }
-  return arr;
-}
+// function solution(arr) {
+//   if (arr.length === arr[0].length) return arr;
+//   if (arr.length > arr[0].length) {
+//     for (let i = 0; i < arr.length; i++) {
+//       arr[i].push(0);
+//     }
+//   } else {
+//     let zeroArr = new Array(arr[0].length).fill(0);
+//     for (let i = 0; i <= arr[0].length - arr.length; i++) {
+//       arr.push(zeroArr);
+//     }
+//   }
+//   return arr;
+// }
