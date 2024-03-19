df69ed8e4261fd053adf9d5e2aa94afc54b015a3
박종민 <qooktree@naver.com>
2024-03-19 20:39:23 +0900

    PGS: 최대공약수와 최소공배수

--- qooktree1/20240320/최대공약수와 최소공배수.js
+++ qooktree1/20240320/최대공약수와 최소공배수.js
@@ -5,8 +5,8 @@
  */
 
 const solution = (n, m) => {
-  let gcd = 1,
-    lcm;
+  let lcm,
+    gcd = 1;
   for (let i = 1; i <= n * m; i++) {
     if (n % i === 0 && m % i === 0) gcd = i;
     if (!lcm && i % n === 0 && i % m === 0) lcm = i;
