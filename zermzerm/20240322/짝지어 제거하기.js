c308fa4a31a914244477dfc256f3fc322f04a910
박종민 <qooktree@naver.com>
2024-03-20 11:06:05 +0900

    PGS: 짝지어 제거하기

---
+++ zermzerm/20240322/짝지어 제거하기.js
@@ -0,0 +1,14 @@
+/**
+ * URL: https://school.programmers.co.kr/learn/courses/30/lessons/12973
+ * 문제 풀이 포인트: 스택
+ * 나의 실수: X
+ */
+
+const solution = (s) => {
+  const stack = [];
+  for (let i = 0; i < s.length; i++) {
+    if (stack[stack.length - 1] === s[i]) stack.pop();
+    else stack.push(s[i]);
+  }
+  return stack.length ? 0 : 1;
+};
