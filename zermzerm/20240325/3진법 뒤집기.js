c988312ecc060bd6da2d5e092056de9847986b38
박경서 <zerm0907@gmail.com>
2024-03-22 12:43:38 +0900

    PGS : 3진법 뒤집기

--- zermzerm/20240325/3진법 뒤집기.js
+++ zermzerm/20240325/3진법 뒤집기.js
@@ -5,7 +5,6 @@
  */
 
 const solution = (n) => {
-  let answer = n.toString(3).split('').reverse().join('');
-  return parseInt(answer, 3);
+  return parseInt(n.toString(3).split('').reverse().join(''), 3);
 };
 console.log(solution(45));
