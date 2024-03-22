4014117e4b7604bc4a5168867b3a235831610a4c
박종민 <qooktree@naver.com>
2024-03-22 11:16:44 +0900

    PGS: 서울에서 김서방 찾기

---
+++ qooktree1/20240325/서울에서 김서방 찾기.js
@@ -0,0 +1,7 @@
+/**
+ * URL: https://school.programmers.co.kr/learn/courses/30/lessons/12919
+ * 문제 풀이 포인트: indexOf
+ * 나의 실수: 지나치게 쉬운 문제였다..
+ */
+
+const solution = (seoul) => `김서방은 ${seoul.indexOf("Kim")}에 있다`;
