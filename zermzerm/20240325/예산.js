8046eb8f2ffb738f5bd3f04076bbf7ec15b4c311
zermzerm <zerm0907@gmail.com>
2024-03-24 18:20:56 +0900

    PGS : 예산

---
+++ zermzerm/20240325/예산.js
@@ -0,0 +1,22 @@
+/**
+ * URL: https://school.programmers.co.kr/learn/courses/30/lessons/12982
+ * 문제 풀이 포인트: 더하기
+ * 나의 실수: 문제 이해, 난 왜이리 길지...
+ */
+
+const solution = (d, budget) => {
+  let cnt = 0,
+    sum = 0;
+  let arr = d.sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    if (sum > budget) return cnt;
+    if (sum === budget) {
+      cnt++;
+      return cnt;
+    }
+    cnt++;
+  }
+};
+
+console.log(solution([2, 2, 3, 3], 10));
