1e600e684d66084623f7bb2312bc7df2dfa03f7a
박경서 <zerm0907@gmail.com>
2024-03-27 10:29:13 +0900

    PGS : 소인수분해

--- qooktree1/20240327/소인수분해.js
+++ qooktree1/20240327/소인수분해.js
@@ -5,14 +5,14 @@
  */
 
 const solution = (n) => {
-  const answer = new Set();
-  while (n > 1) {
-    for (let i = 2; i <= n; i++) {
-      if (n % i === 0) {
-        answer.add(i);
-        n /= i;
-        break;
-      }
+  let answer = new Set();
+  let index = 2;
+  while (n != 1) {
+    if (n % index === 0) {
+      n /= index;
+      answer.add(index);
+    } else {
+      index++;
     }
   }
   return [...answer];
