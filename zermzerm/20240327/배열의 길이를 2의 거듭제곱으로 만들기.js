8bad31898ff68a5eadf6da8abb6c9ff897e89b51
박경서 <zerm0907@gmail.com>
2024-03-27 10:10:16 +0900

    PGS : 배열의 길이를 2의 거듭제곱으로 만들기

--- zermzerm/20240327/배열의 길이를 2의 거듭제곱으로 만들기.js
+++ zermzerm/20240327/배열의 길이를 2의 거듭제곱으로 만들기.js
@@ -7,12 +7,10 @@
 const solution = (arr) => {
   let answer = [...arr];
   let n = 1;
-  let cnt = 0;
   while (arr.length > n) {
     n *= 2;
-    cnt++;
   }
-  for (let i = 0; i < 2 ** cnt - arr.length; i++) {
+  for (let i = 0; i < n - arr.length; i++) {
     answer.push(0);
   }
   return answer;
