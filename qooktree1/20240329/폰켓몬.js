61f9677d83e9fd6165096a1bf7672663f75f33e7
박종민 <qooktree@naver.com>
2024-03-29 10:29:58 +0900

    PGS: 폰켓몬

--- qooktree1/20240329/폰켓몬.js
+++ qooktree1/20240329/폰켓몬.js
@@ -4,8 +4,4 @@
  * 나의 실수: X
  */
 
-const solution = (nums) => {
-  const numSet = new Set();
-  for (const num of nums) numSet.add(num);
-  return Math.min(nums.length / 2, numSet.size);
-};
+const solution = (nums) => Math.min(nums.length / 2, new Set(nums).size);
