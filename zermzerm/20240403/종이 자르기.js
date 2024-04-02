eeaef694e2675abe06c0796206c402f83cd1e9dc
박경서 <zerm0907@gmail.com>
2024-04-03 02:09:19 +0900

    PGS : 종이 자르기

---
+++ zermzerm/20240403/종이 자르기.js
@@ -0,0 +1,12 @@
+/**
+ * URL: https://school.programmers.co.kr/learn/courses/30/lessons/120922
+ * 문제 이름 : 종이 자르기
+ * 문제 풀이 포인트: 잉?
+ * 나의 실수:
+ */
+
+function solution(M, N) {
+  return M * N - 1;
+}
+
+console.log(solution(2, 2));
