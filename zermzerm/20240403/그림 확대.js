7c257c8bf0fe9d41640b80a1fcde248ebccaad5e
박경서 <zerm0907@gmail.com>
2024-04-03 02:09:11 +0900

    PGS : 그림 확대

---
+++ zermzerm/20240403/그림 확대.js
@@ -0,0 +1,34 @@
+/**
+ * URL: https://school.programmers.co.kr/learn/courses/30/lessons/181836
+ * 문제 이름 : 그림 확대
+ * 문제 풀이 포인트: concat = 배열에 새로운 요소나 배열을 추가하여 새로운 배열을 반환
+ * 나의 실수:
+ */
+
+function solution(picture, k) {
+  let answer = [];
+
+  for (let i = 0; i < picture.length; i++) {
+    let tmp = '';
+    for (let j = 0; j < picture[i].length; j++) {
+      tmp += picture[i][j].repeat(k);
+    }
+    answer = answer.concat(Array(k).fill(tmp));
+  }
+  return answer;
+}
+
+console.log(
+  solution(
+    [
+      '.xx...xx.',
+      'x..x.x..x',
+      'x...x...x',
+      '.x.....x.',
+      '..x...x..',
+      '...x.x...',
+      '....x....',
+    ],
+    2
+  )
+);
