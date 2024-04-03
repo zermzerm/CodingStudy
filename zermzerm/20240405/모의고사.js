7b31b84d8ae043b9f105e642da0c6bc8df24fd85
박경서 <zerm0907@gmail.com>
2024-04-03 12:48:15 +0900

    PGS : 모의고사

--- zermzerm/20240405/모의고사.js
+++ zermzerm/20240405/모의고사.js
@@ -9,7 +9,7 @@ const one = [1, 2, 3, 4, 5];
 const two = [2, 1, 2, 3, 2, 4, 2, 5];
 const three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
 
-const compete = (answers, arr, cnt, k) => {
+const compare = (answers, arr, cnt, k) => {
   for (let i = 0; i < answers.length; i++) {
     let j = i % arr.length;
     if (answers[i] === arr[j]) cnt[k]++;
@@ -19,9 +19,9 @@ const compete = (answers, arr, cnt, k) => {
 const solution = (answers) => {
   const cnt = [0, 0, 0];
   const answer = [];
-  compete(answers, one, cnt, 0);
-  compete(answers, two, cnt, 1);
-  compete(answers, three, cnt, 2);
+  compare(answers, one, cnt, 0);
+  compare(answers, two, cnt, 1);
+  compare(answers, three, cnt, 2);
   let max = Math.max(...cnt);
   for (let i = 0; i < cnt.length; i++) {
     if (cnt[i] === max) answer.push(i + 1);
