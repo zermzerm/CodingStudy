78acbe967e0e0c49f76a930b7af475c9d6cb4d60
Soeun Kim <summerkim0825@gmail.com>
2024-04-05 00:01:41 +0900

    PGS: 문자열 다루기

---
+++ summerkimm/20240405/문자열 다루기 기본.js
@@ -0,0 +1,21 @@
+/**
+ * URL: https://school.programmers.co.kr/learn/courses/30/lessons/12918
+ * 문제 풀이 과정
+ * 1. 길이가 조건에 맞지 않는 경우 false로 리턴
+ * 2. for 문으로 문자열 돌면서 number가 아니면 false 리턴 
+ */
+
+function solution(s) {
+
+
+//   if (s.length !== 4 || s.length !== 6) { // 안 되는 이유????
+  if (!(s.length === 4 || s.length === 6)) {
+    return false;
+  }
+  for (let i = 0; i < s.length; i++) {
+    if (isNaN(s[i])) {
+      return false;
+    }
+  }
+  return true;
+}
