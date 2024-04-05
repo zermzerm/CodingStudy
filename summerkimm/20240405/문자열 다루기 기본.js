24bfc22a011a31ce8eb13f3d7f6dbbbf33ffbb26
Soeun Kim <summerkim0825@gmail.com>
2024-04-05 14:48:39 +0900

    PGS: 문자열 다루기 기본 수정

--- summerkimm/20240405/문자열 다루기 기본.js
+++ summerkimm/20240405/문자열 다루기 기본.js
@@ -7,8 +7,7 @@
 
 function solution(s) {
 
-//   if (s.length !== 4 || s.length !== 6) { // 안 되는 이유????
-  if (!(s.length === 4 || s.length === 6)) {
+  if (s.length !== 4 && s.length !== 6) {
     return false;
   }
   for (let i = 0; i < s.length; i++) {
