cefb99063f919e03cddb977e413dcd98d2b50107
박경서 <zerm0907@gmail.com>
2024-04-05 12:07:04 +0900

    PGS : 카펫

--- zermzerm/20240408/카펫.js
+++ zermzerm/20240408/카펫.js
@@ -7,7 +7,7 @@
 
 //다시 푼 코드
 function solution(brown, yellow) {
-  for (let i = 1; i <= yellow; i++) {
+  for (let i = 1; i <= Math.ceil(yellow / 2); i++) {
     if (yellow % i === 0) {
       if ((yellow / i) * 2 + i * 2 + 4 === brown)
         return [yellow / i + 2, i + 2];
