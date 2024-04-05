f170e21f304dfd0482b122fba40f96fd550f7668
박경서 <zerm0907@gmail.com>
2024-04-05 11:57:43 +0900

    PGS : 카펫

---
+++ zermzerm/20240408/카펫.js
@@ -0,0 +1,46 @@
+/**
+ * URL: https://school.programmers.co.kr/learn/courses/30/lessons/42842
+ * 문제 이름 :
+ * 문제 풀이 포인트:
+ * 나의 실수:
+ */
+
+//다시 푼 코드
+function solution(brown, yellow) {
+  for (let i = 1; i <= yellow; i++) {
+    if (yellow % i === 0) {
+      if ((yellow / i) * 2 + i * 2 + 4 === brown)
+        return [yellow / i + 2, i + 2];
+    }
+  }
+}
+
+console.log(solution(20, 15));
+
+//
+
+//예전에 푼 코드
+// function solution(brown, yellow) {
+//     var answer = [];
+//     var br = brown - 4;
+//     if (yellow == 1) {
+//       return [3, 3];
+//     }
+//     for (i = 1; i < yellow; i++) {
+//       var a = 0,
+//         b = 0;
+//       if (yellow % i == 0) {
+//         a = i;
+//         b = yellow / i;
+//         if (answer.filter((v) => a === v).length == 1) {
+//           break;
+//         }
+//         if (a * 2 + b * 2 == br) {
+//           answer.push(b, a);
+//         }
+//       }
+//     }
+//     answer[0] = answer[0] + 2;
+//     answer[1] = answer[1] + 2;
+//     return answer;
+//   }
