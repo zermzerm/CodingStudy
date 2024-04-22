0dad55b19ccb7dea2d67fe18be23380f37b6db25
박종민 <qooktree@naver.com>
2024-04-22 10:43:57 +0900

    PGS: 다음 큰 수수자

---
+++ qooktree1/20240423/다음 큰 숫자.js
@@ -0,0 +1,16 @@
+/**
+ * URL: https://school.programmers.co.kr/learn/courses/30/lessons/12911
+ * 문제 풀이 포인트: 조건 1,2, 3을 순서대로 구현한다.
+ * 나의 실수: X
+ * [ 풀이 과정 ]
+ * 1. toString을 이용하여 2진수를 만든 후 1의 개수를 비교한다.
+ */
+
+const countOnes = (num) => [...num.toString(2)].filter((c) => c === "1").length;
+
+const solution = (n) => {
+  const oneCnt = countOnes(n);
+  for (let i = n + 1; i <= 1000000; i++) {
+    if (oneCnt === countOnes(i)) return i;
+  }
+};
