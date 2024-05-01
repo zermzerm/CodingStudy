f42219c2b4858d10bbd80d0b3aa96e2076191d71
zermzerm <zerm0907@gmail.com>
2024-05-01 19:06:21 +0900

    PGS : 소수 만들기

---
+++ zermzerm/20240503/소수 만들기.js
@@ -0,0 +1,41 @@
+/**
+ * URL: https://school.programmers.co.kr/learn/courses/30/lessons/12977
+ * 문제 이름 : 소수 만들기
+ * 문제 풀이 포인트:
+ * 나의 실수:
+ */
+
+// 조합 함수(배열중 몇개를 선택해서 나올수 있는 조합 구하기)
+function combination(arr, n) {
+  let cnt = 0;
+  function backtrack(tmp, start) {
+    if (tmp.length === n) {
+      let sum = tmp.reduce((acc, cur) => acc + cur, 0);
+      if (prime(sum)) cnt++;
+      return;
+    }
+    for (let i = start; i < arr.length; i++) {
+      tmp.push(arr[i]);
+      backtrack(tmp, i + 1);
+      tmp.pop();
+    }
+  }
+
+  // 조합 시작
+  backtrack([], 0);
+  return cnt;
+}
+
+// 소수인지 판별
+function prime(number) {
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) return false;
+  }
+  return true;
+}
+
+function solution(nums) {
+  return combination(nums, 3);
+}
+
+console.log(solution([1, 2, 7, 6, 4]));
