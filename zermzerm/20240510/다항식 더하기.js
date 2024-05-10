69487d6403c73c72f8faddb91cc28456ede0a48e
박경서 <zerm0907@gmail.com>
2024-05-10 20:01:01 +0900

    PGS : 다항식 더하기

--- zermzerm/20240510/다항식 더하기.js
+++ zermzerm/20240510/다항식 더하기.js
@@ -7,37 +7,36 @@
 
 // 다시 푼 코드
 function calc(prev, num) {
-  if (prev === "+") return +num;
-  else if (prev === "-") return -num;
+  if (prev === '+') return +num;
+  else if (prev === '-') return -num;
   else return +num;
 }
 
 function solution(polynomial) {
-  const arr = polynomial.split(" ");
-  let xValue = 0,
+  const arr = polynomial.split(' ');
+  let xValue,
     numValue = 0;
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i].includes("x")) {
-      let num = arr[i].replace(/[^0-9]/g, "");
+    if (arr[i].includes('x')) {
+      let num = arr[i].replace(/[^0-9]/g, '');
       if (num === 0) num = 1;
-      if (arr[i] === "-x") num = -1;
+      if (arr[i] === '-x') num = -1;
       xValue += calc(arr[i - 1], num);
     } else if (Number.isInteger(+arr[i])) {
       let num = +arr[i];
       numValue += calc(arr[i - 1], num);
     }
   }
-  console.log(xValue);
-  if (xValue === 1) xValue = "";
-  if (xValue === -1) xValue = "- ";
+  if (xValue === 1) xValue = '';
+  if (xValue === -1) xValue = '- ';
   if (xValue === 0) return `${numValue}`;
   if (numValue === 0) return `${xValue}x`;
   return `${xValue}x ${
-    numValue < 0 ? "- " + Math.abs(numValue) : "+ " + numValue
+    numValue < 0 ? '- ' + Math.abs(numValue) : '+ ' + numValue
   }`;
 }
 
-console.log(solution("- 3x - 1"));
+console.log(solution('- 3x - 1'));
 
 /* 예전에 푼 코드
 function solution(polynomial) {
