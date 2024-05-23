06fc9178c4a84719c60d0eb40c77110223fc4b5a
zermzerm <zerm0907@gmail.com>
2024-05-23 16:30:28 +0900

    test

--- zermzerm/짤짤이/test.js
+++ zermzerm/짤짤이/test.js
@@ -1,8 +1,15 @@
-// let remaining = "01234";
+/* let remaining = "01234";
 // for (let i = 0; i < remaining.length; i++) {
 //   let tmp = remaining.slice(0, i) + remaining.slice(i + 1);
 //   console.log(tmp);
-// }
+  } */
+
+/* endsWith = 매개변수로 끝나는지 확인 / startsWith = 매개변수로 시작되는지 확인
+// function solution(my_string, is_suffix) {
+//   return my_string.endsWith(is_suffix) ? 1 : 0
+//   }
+
+console.log(solution("banana","ana")) */
 
 function solution(num_list) {
   const answer = [0, 0];
