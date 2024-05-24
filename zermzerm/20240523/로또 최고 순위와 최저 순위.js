c67b6900b114db6bd4a9cf575b6f34a8bfb76efb
박경서 <zerm0907@gmail.com>
2024-05-24 20:06:03 +0900

    PGS : 로또의 최고 순위와 최저 순위

--- zermzerm/20240523/로또 최고 순위와 최저 순위.js
+++ zermzerm/20240523/로또 최고 순위와 최저 순위.js
@@ -12,11 +12,10 @@ function solution(lottos, win_nums) {
   for (let i = 0; i < lottos.length - zeroCount; i++) {
     if (win_nums.includes(lottos[i])) sameCount++;
   }
-  if (zeroCount === 6 && sameCount === 0) return [1, 6];
   return [
     7 - (sameCount + zeroCount) > 6 ? 6 : 7 - (sameCount + zeroCount),
     7 - sameCount > 6 ? 6 : 7 - sameCount,
   ];
 }
 
-console.log(solution([1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12]));
+console.log(solution([0, 0, 0, 0, 0, 0], [7, 8, 9, 10, 11, 12]));
