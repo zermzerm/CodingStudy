0f3b473306b26af476572720e5aea6a0c53eae38
unknown <zerm0907@gmail.com>
2024-09-30 19:44:20 +0900

    순열 추가

--- zermzerm/짤짤이/test.js
+++ zermzerm/짤짤이/test.js
@@ -1,3 +1,24 @@
+
+/*
+1 = 1
+
+2 = 2
+
+3 = 3
+
+4 = 5
+
+5 = 8
+
+6 = 13
+
+7 = 21
+*/ 
+
+// let permutations = permute([1, 1, 2]);
+// console.log(permutations);
+
+
 /* let remaining = "01234";
 // for (let i = 0; i < remaining.length; i++) {
 //   let tmp = remaining.slice(0, i) + remaining.slice(i + 1);
@@ -11,16 +32,18 @@
 
 console.log(solution("banana","ana")) */
 
-function solution(num_list) {
-  const answer = [0, 0];
-  for (let i = 0; i < num_list.length; i++) {
-    if (i % 2 === 0) {
-      answer[0] += num_list[i];
-    } else {
-      answer[1] += num_list[i];
-    }
-  }
-  return Math.max(...answer);
-}
 
-console.log(solution([4, 2, 6, 1, 7, 6]));
+
+// function solution(num_list) {
+//   const answer = [0, 0];
+//   for (let i = 0; i < num_list.length; i++) {
+//     if (i % 2 === 0) {
+//       answer[0] += num_list[i];
+//     } else {
+//       answer[1] += num_list[i];
+//     }
+//   }
+//   return Math.max(...answer);
+// }
+
+// console.log(solution([4, 2, 6, 1, 7, 6]));
--- zermzerm/짤짤이/조합과 순열 코드.js
+++ zermzerm/짤짤이/조합과 순열 코드.js
@@ -1,25 +1,25 @@
 // 조합
-function getCombinations(arr, n) {
-  let results = [];
+// function getCombinations(arr, n) {
+//   let results = [];
 
-  if (n === 1) {
-    return arr.map((item) => [item]);
-  }
+//   if (n === 1) {
+//     return arr.map((item) => [item]);
+//   }
 
-  arr.forEach((current, index) => {
-    const smallerCombinations = getCombinations(arr.slice(index + 1), n - 1);
-    smallerCombinations.forEach((combination) => {
-      results.push([current, ...combination]);
-    });
-  });
+//   arr.forEach((current, index) => {
+//     const smallerCombinations = getCombinations(arr.slice(index + 1), n - 1);
+//     smallerCombinations.forEach((combination) => {
+//       results.push([current, ...combination]);
+//     });
+//   });
 
-  return results;
-}
+//   return results;
+// }
 
-const array = [1, 2, 3, 5, 7, 9];
-const n = 3;
-const combinations = getCombinations(array, n);
-console.log(combinations);
+// const array = [1, 2, 3, 5, 7, 9];
+// const n = 3;
+// const combinations = getCombinations(array, n);
+// console.log(combinations);
 
 // 순열
 // function getPermutations(arr, n) {
@@ -40,7 +40,35 @@ console.log(combinations);
 //   return results;
 // }
 
-// const array = [1, 2, 3, 5, 7, 9];
+// const array = [1, 2,1];
 // const n = 3;
 // const permutations = getPermutations(array, n);
 // console.log(permutations);
+
+//순열2
+function permute(arr) {
+  let result = [];
+
+  const permuteHelper = (currentArr, remainingArr) => {
+      if (remainingArr.length === 0) {
+          result.push(currentArr);
+      } else {
+          let unique = new Set();
+          for (let i = 0; i < remainingArr.length; i++) {
+              if (!unique.has(remainingArr[i])) {
+                  unique.add(remainingArr[i]);
+                  permuteHelper(
+                      currentArr.concat(remainingArr[i]), 
+                      remainingArr.slice(0, i).concat(remainingArr.slice(i + 1))
+                  );
+              }
+          }
+      }
+  };
+
+  permuteHelper([], arr);
+  return result;
+}
+
+let permutations = permute([1, 1, 2]);
+console.log(permutations);
