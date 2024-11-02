dfdc0eb864084ba8161cab862553c0e511138b23
unknown <zerm0907@gmail.com>
2024-11-02 20:23:09 +0900

    짤짤이

---
+++ zermzerm/짤짤이/뒤에 있는 큰 수 찾기.js
@@ -0,0 +1,28 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/154539
+// 뒤에서 부터 찾기?
+function solution(numbers) {
+  let maxnum = 0;
+  let stack = [];
+  for (let i = numbers.length - 1; i >= 0; i--) {
+    if (maxnum <= numbers[i]) {
+      maxnum = numbers[i];
+      answer.push(-1);
+      stack = [];
+      stack.push(numbers[i]);
+    } else {
+      let j = 0;
+      while (1) {
+        if (numbers[i] < stack[j]) {
+          answer.push(stack[j]);
+          stack.unshift(numbers[i]);
+          break;
+        } else {
+          stack.shift();
+        }
+      }
+    }
+  }
+  answer.reverse();
+
+  return answer;
+}
